91aef461dd1d8112ffc8c6190c34b78297629967
Michael Sjoeberg <mail@michaelsjoeberg.com>
2018-10-28 16:46:41 +0100

    note controller, note seed, and scss

--- notes-app/config/routes.rb
+++ notes-app/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :books do
-        # notes as a resource of a resource
-        resources :notes
+        # notes as a resource of a resource (create and destroy only)
+        resources :notes, only: [:create, :destroy]
     end
     
     root to: "books#index"
