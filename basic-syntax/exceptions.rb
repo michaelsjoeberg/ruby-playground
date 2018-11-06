bf2c8e3e54bb3c280722b566c235932e46bef7e0
Michael Sjoeberg <mail@michaelsjoeberg.com>
2018-11-06 23:38:40 +0100

    updated ruby playground structure

---
+++ basic-syntax/exceptions.rb
@@ -0,0 +1,32 @@
+begin
+    # do something
+
+rescue Exception => e
+    puts e.message
+    # do something
+
+end
+
+# check if something exists
+if File.exists? 'no_file.txt'
+    # handle file
+
+else
+    # handle no file
+
+end
+
+# unless something meet condition
+unless @variable
+    # true
+
+else
+    # false
+
+end
+
+'''
+Michael Sjoeberg
+2018-11-06
+https://github.com/michaelsjoeberg/ruby-playground/blob/master/basic-syntax/exceptions.rb
+'''
